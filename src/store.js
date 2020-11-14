c76146d65e848cc1b9e201c9efa36d7a11c3ef7c
Yael Yañez <yaelyanez112@gmail.com>
2020-11-14 15:13:06 -0600

    Commented Redux Dev Tools

--- src/store.js
+++ src/store.js
@@ -2,15 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const env = process.env.NODE_ENV;
-
 export default createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    env === 'development'
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
